a52d7009ca6d29776622bef17892d00983a112a5
Diyorbek <rajabov.diyorbek.it@gmail.com>
2023-01-03 16:52:33 +0500

    ///

--- src/components/CreateContact.vue
+++ src/components/CreateContact.vue
@@ -20,7 +20,7 @@
         >
         <input v-model="email" type="email" class="form-control" required />
       </div>
-      <router-link to="/">
+      <router-link to="/" :disabled="disabled()">
         <button
           @click="addContact"
           class="btn btn-primary"
