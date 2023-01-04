518ac3c39d4a57ca22a62af9439269f71b300cfd
Diyorbek <rajabov.diyorbek.it@gmail.com>
2023-01-04 13:05:08 +0500

    editcomponent

--- src/components/view/EditContact.vue
+++ src/components/view/EditContact.vue
@@ -1,27 +1,40 @@
 <template>
-  <form class="container-lg mt-5" @submit.prevent>
+  <form class="container-lg mt-5" @submit.prevent="editContact">
     <h1>Изменить данные контакта</h1>
     <div>
       <div class="mb-3">
         <label class="form-label">Введите Имя</label>
-        <input v-model="contact.name" type="text" class="form-control" />
+        <input
+          v-model="contact.name"
+          type="text"
+          class="form-control"
+          required
+        />
       </div>
       <div class="mb-3">
         <label class="form-label">Введите Номер </label>
-        <input v-model="contact.number" type="number" class="form-control" />
+        <input
+          v-model="contact.number"
+          type="number"
+          class="form-control"
+          required
+        />
       </div>
       <div class="mb-3">
         <label class="form-label">Введите Email </label>
-        <input v-model="contact.email" type="email" class="form-control" />
+        <input
+          v-model="contact.email"
+          type="email"
+          class="form-control"
+          required
+        />
       </div>
       <ul
         class="list-group list-group-horizontal d-flex justify-content-end mt-2"
       >
-        <router-link to="/">
-          <button class="btn btn-primary" @click="editContact" type="submit">
-            Сохранить Изменения
-          </button>
-        </router-link>
+        <button class="btn btn-primary" type="submit">
+          Сохранить Изменения
+        </button>
         <router-link to="/">
           <button class="btn btn-danger ms-2" type="button">Отменить</button>
         </router-link>
@@ -31,6 +44,8 @@
 </template>
 
 <script>
+import router from "@/router";
+
 export default {
   name: "EditContact",
   data() {
@@ -48,6 +63,7 @@ export default {
       const contacts = JSON.parse(localStorage.getItem("contacts")) || [];
       contacts[Number(this.$route.params.indx)] = this.contact;
       localStorage.setItem("contacts", JSON.stringify(contacts));
+      router.push("/");
     },
   },
 };
